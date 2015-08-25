ee8cfa510c021dfca7864a1c0998bfc4f282b5d3
Christoph Glur <ceglur@Turbine.lakefour.com>
2015-08-25 21:19:22 +0200

    refs #1

--- tests/testthat/test-bdlParseFile.R
+++ tests/testthat/test-bdlParseFile.R
@@ -21,3 +21,27 @@ test_that("BdlParseFile Simple sample1", {
   
   
 })
+
+
+
+test_that("BdlParseFile History Multi", {
+  
+  
+  fileName <- system.file("extdata", "historyReply1.out", package="datalicenseR")
+  bdlResponse <- readChar(fileName, file.info(fileName)$size)
+  
+  bdlResponseDf <- GetHistoryParser(bdlResponse)
+  expect_equal(class(bdlResponseDf), 'data.frame')
+  expect_equal(nrow(bdlResponseDf), 2)
+  expect_equal(rownames(bdlResponseDf), c('IBM US Equity', 'INDU Index'))
+  
+  expect_equal(ncol(bdlResponseDf), 3)
+  expect_equal(colnames(bdlResponseDf), c('ERROR_CODE', 'PX_LAST', 'PX_CLOSE'))
+  
+  expect_equal(bdlResponseDf[ , 1], c(0,0))
+  expect_equal(bdlResponseDf[ , 2], c(105.23, 17840.52))
+  expect_equal(bdlResponseDf[ , 3], c(105.29, 17839.62))
+  
+  
+  
+})
